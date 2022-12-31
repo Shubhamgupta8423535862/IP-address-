3c70a7ca17011367b75cb48840aa833654c11402
Shubhamgupta8423535862 <115138851+Shubhamgupta8423535862@users.noreply.github.com>
2022-12-31 08:07:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function getIp() {
+    $.getJSON("https://api.ipify.org?format=json", function (data) {
+        $("#ip-address").html(data.ip);
+        const userIp = data.ip
+        console.log(userIp)
+
+    })
+}
+getIp();
