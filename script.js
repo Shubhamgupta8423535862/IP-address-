ebdb24789ab29b74f78965fdce1520b2f47cf236
Shubhamgupta8423535862 <115138851+Shubhamgupta8423535862@users.noreply.github.com>
2022-12-31 10:55:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function getIp() {
-    $.getJSON("https://api.ipify.org?format=json", function (data) {
-        $("#ip-address").html(data.ip);
-        const userIp = data.ip
-        console.log(userIp)
-
-    })
-}
-getIp();
